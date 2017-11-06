90167d4b4d948c9ea7cede9ed6b091057b4cfb3f
johnskpaul <jk.paul@pitt.edu>
2017-11-06 10:32:26 -0500

    commit -m 'iBRIC TEST 1162017'

---
+++ src/test/java/com/dbmi/iBRIC/iBRIC_LifeAtIbric.java
@@ -0,0 +1,67 @@
+package com.dbmi.iBRIC;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+
+public class iBRIC_LifeAtIbric extends iBRIC_WebLogin {
+  @Test
+  public void testIbricLifeAtIbric() throws Exception {
+	  testIbricLogin();
+	  driver.findElement(By.xpath(".//*[@id='navigation']/ul/li[6]/a")).click();
+	  Thread.sleep(2000);
+	  List<WebElement> linksList = clickableLinks(driver);
+		for (WebElement link : linksList) {
+			String href = link.getAttribute("href");
+			try {
+				System.out.println("URL " + href + " returned " + linkStatus(new URL(href)));
+			}
+			catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+
+	}
+	
+	public static List<WebElement> clickableLinks(WebDriver driver) {
+		List<WebElement> linksToClick = new ArrayList<WebElement>();
+		List<WebElement> elements = driver.findElements(By.tagName("a"));
+		elements.addAll(driver.findElements(By.tagName("img")));
+		
+		for (WebElement e : elements) {
+			if (e.getAttribute("href") != null){
+				linksToClick.add(e);
+			}
+		}
+		return linksToClick;
+	}
+	
+	public static String linkStatus(URL url) {
+		// http://download.java.net/jdk7/archive/b123/docs/api/java/net/HttpURLConnection.html#getResponseMessage%28%29
+		try {
+			HttpURLConnection http = (HttpURLConnection) url.openConnection();
+			http.connect();
+			String responseMessage = http.getResponseMessage();
+			http.disconnect();
+			return responseMessage;
+		}
+		catch (Exception e) {
+			return e.getMessage();
+		}
+}
+
+  @AfterMethod
+  public void afterMethod() {
+	  driver.quit();
+  }
+
+}
---
+++ src/test/java/com/dbmi/iBRIC/iBRIC_WebLogin.java
@@ -0,0 +1,48 @@
+package com.dbmi.iBRIC;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class iBRIC_WebLogin {
+  public WebDriver driver;
+  public String baseUrl;
+  public static WebElement element = null;
+  
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+	System.setProperty("webdriver.gecko.driver", "C:\\Program Files\\Apache\\geckodriver.exe");
+    driver = new FirefoxDriver();
+    baseUrl = "http://dev.ibric.dbmi.pitt.edu/";
+//    baseUrl = "https://act-network.org/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+//  @Test
+  public void testIbricLogin() throws Exception {
+    driver.get(baseUrl + "user/");
+    driver.findElement(By.id("edit-name")).clear();
+    driver.findElement(By.id("edit-name")).sendKeys("admin");
+    driver.findElement(By.id("edit-pass")).clear();
+    driver.findElement(By.id("edit-pass")).sendKeys("dbmi2016ibr");
+    driver.findElement(By.id("edit-submit")).click();
+    Thread.sleep(200);
+//    driver.findElement(By.linkText("Login")).click();
+  
+  }
+//  protected void navigateToActPage(WebDriver driver) {
+//		driver.findElement(By.linkText("Overview")).click();
+//		element = driver.findElement(By.linkText("Week 1: March 17, 2016"));
+//		element.click();
+//  }
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+//    driver.quit();
+    }
+  }
